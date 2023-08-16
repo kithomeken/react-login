2af0f39911e8ff35d02c2b5acb43ec377e738e07
Kennedy K. Mutiso [ICT] <kmutiso@CO-OPBANK.CO.KE>
2023-08-16 12:15:38 +0300

    Global Registry

---
+++ src/global/ConstantsRegistry.tsx
@@ -0,0 +1,18 @@
+// Constants Registry
+export const APP_NAME = 'Beska'
+export const APP_THEME = 'green'
+export const COOKIE_OPTIONS = {path: '/', secure: true, sameSite: 'none'}
+
+
+/*
+* Encryption Keys
+* */
+export const KEY_BASE = '_4Cx8rIks0p'
+export const IV_BASE = '_5ZcL35EsJh'
+
+
+/*
+* Local storage keys
+* */
+export const KEY_ACCOUNT_INFO = '_b58gwzJbKi'
+export const KEY_TIMEZONE = '_R3NRGbvlkeYt'
