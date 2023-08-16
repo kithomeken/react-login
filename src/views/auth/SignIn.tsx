ec429f6cfac114cfe82c27d16d786363e8caa6dc
Kennedy K. Mutiso [ICT] <kmutiso@CO-OPBANK.CO.KE>
2023-08-16 12:18:28 +0300

    Sign In Authentication Temp

---
+++ src/views/auth/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+import React, { useState } from "react";
+
+export const SignIn = () => {
+
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>Sign In</title>
+            </Helmet>
+
+        </React.Fragment>
+    )
+}
